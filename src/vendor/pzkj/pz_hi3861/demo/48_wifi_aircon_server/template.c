73b9070aa5b52dbbed8baf12ed1bd052f872a55d
Ruanye <2307298848@qq.com>
2026-06-28 16:04:33 +0800

    test: add minimal aircon uart smoke demo

--- src/vendor/pzkj/pz_hi3861/demo/48_wifi_aircon_server/template.c
+++ src/vendor/pzkj/pz_hi3861/demo/48_wifi_aircon_server/template.c
@@ -4,68 +4,49 @@
 #include "cmsis_os2.h"
 #include "ohos_init.h"
 
-#include "wifi_aircon_server.h"
+#include "aircon_ctrl.h"
 
-static osThreadId_t g_aircon_server_task_id = NULL;
-static osThreadId_t g_aircon_status_task_id = NULL;
+#define AIRCON_TEST_DELAY_S 3
 
-static void aircon_server_task(void)
+static osThreadId_t g_aircon_test_task_id = NULL;
+
+static void aircon_test_task(void)
 {
-    (void)wifi_aircon_server_start();
-}
+    int ret;
 
-static void aircon_status_task(void)
-{
-    wifi_aircon_server_status_t status;
+    printf("[ac-test] power-on smoke test starts in %d seconds\r\n", AIRCON_TEST_DELAY_S);
+    sleep(AIRCON_TEST_DELAY_S);
 
-    while (1) {
-        wifi_aircon_server_get_status(&status);
-        printf("[ac-status] wifi_connecting=%u wifi_connected=%u tcp_listening=%u "
-            "client_connected=%u state=%u baud=%u last_action=%s ip=%s port=%u last_error=%d\r\n",
-            status.wifi_connecting,
-            status.wifi_connected,
-            status.tcp_listening,
-            status.tcp_client_connected,
-            status.ac_state,
-            status.uart_baud,
-            status.last_action,
-            status.ip,
-            status.server_port,
-            status.last_error);
-        sleep(5);
+    ret = aircon_ctrl_init();
+    if (ret != AIRCON_CTRL_RET_OK) {
+        printf("[ac-test] uart init failed, ret=%d\r\n", ret);
+        return;
     }
+
+    printf("[ac-test] sending one OFF raw code now\r\n");
+    ret = aircon_ctrl_power_off();
+    printf("[ac-test] send finished, ret=%d, state=%s\r\n",
+        ret, aircon_ctrl_get_state_text());
 }
 
 static void wifi_aircon_demo(void)
 {
-    osThreadAttr_t server_task_options = {0};
-    osThreadAttr_t status_task_options = {0};
+    osThreadAttr_t test_task_options = {0};
 
-    printf("PZKJ Hi3861 Wi-Fi aircon control demo\r\n");
-    printf("AC power on/off use direct raw IR code over UART1\r\n");
+    printf("PZKJ Hi3861 aircon UART smoke test\r\n");
+    printf("[ac-test] wifi/tcp disabled; auto-send one verified raw code on boot\r\n");
+    printf("[ac-test] wiring: GPIO0->module RX, GPIO1->module TX, GND shared\r\n");
 
-    server_task_options.name = "airconSrv";
-    server_task_options.stack_size = 1024 * 10;
-    server_task_options.priority = osPriorityNormal;
+    test_task_options.name = "airconTest";
+    test_task_options.stack_size = 1024 * 4;
+    test_task_options.priority = osPriorityNormal;
 
-    g_aircon_server_task_id = osThreadNew((osThreadFunc_t)aircon_server_task,
-        NULL, &server_task_options);
-    if (g_aircon_server_task_id != NULL) {
-        printf("Create aircon server task success, id=%d\r\n", g_aircon_server_task_id);
+    g_aircon_test_task_id = osThreadNew((osThreadFunc_t)aircon_test_task,
+        NULL, &test_task_options);
+    if (g_aircon_test_task_id != NULL) {
+        printf("[ac-test] create task success, id=%d\r\n", g_aircon_test_task_id);
     } else {
-        printf("Create aircon server task failed\r\n");
-    }
-
-    status_task_options.name = "airconStat";
-    status_task_options.stack_size = 1024 * 4;
-    status_task_options.priority = osPriorityBelowNormal;
-
-    g_aircon_status_task_id = osThreadNew((osThreadFunc_t)aircon_status_task,
-        NULL, &status_task_options);
-    if (g_aircon_status_task_id != NULL) {
-        printf("Create aircon status task success, id=%d\r\n", g_aircon_status_task_id);
-    } else {
-        printf("Create aircon status task failed\r\n");
+        printf("[ac-test] create task failed\r\n");
     }
 }
 
